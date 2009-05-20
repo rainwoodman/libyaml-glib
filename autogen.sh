457c6b3380ebd1d494a41c8e29c053d4bee2ceaa
Feng Yu <fengyu@dhcp5-240.iucf.indiana.edu>
2009-05-20 13:23:10 -0400

    Repackage libyaml-glib. Move libyaml into a subpackage of libyaml.

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,7 @@ libtoolize --force --automake
 autoheader
 automake --add-missing
 autoconf
+(cd libyaml; autoreconf -fvi;)
 if ! test x$1 == x--no-configure; then
 ./configure --enable-maintainer-mode $*
 fi;
