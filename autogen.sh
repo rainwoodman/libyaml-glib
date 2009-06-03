182679f5eccfcf317a73027e78b65aa4dd819cd8
Feng Yu <fengyu@dhcp5-240.iucf.indiana.edu>
2009-06-03 14:08:33 -0400

    Add -1.0 suffix to package names

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,14 @@
-mkdir -p config
-aclocal -I config
+M4_BOOTSTRAPS="vala.m4 valadoc.m4 dolt.m4"
+M4_BOOTSTRAP_SRC_PATH=${M4_BOOTSTRAP_SRC_PATH:-../autotools}
+mkdir -p autotools
+for i in $M4_BOOTSTRAPS; do
+	if [ -f $M4_BOOTSTRAP_SRC_PATH/$i ]; then
+		cp $M4_BOOTSTRAP_SRC_PATH/$i autotools
+	else
+		echo Warning: file $i is not found, set M4_BOOTSTRAP_SRC_PATH.
+	fi
+done;
+aclocal -I autotools
 libtoolize --force --automake
 autoheader
 automake --add-missing
